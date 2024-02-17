643be0c0871710450b68428c5afc08853120442a
Kim Sol <93749644+kimsol729@users.noreply.github.com>
2024-02-17 15:45:41 +0900

    copy basic deep operator codes

---
+++ src/data_generator.py
@@ -0,0 +1,136 @@
+#%%
+import numpy as np
+from jax import random, jit
+from jax.config import config
+from functools import partial
+from torch.utils import data
+from model_sir import solve_SIR
+import pickle
+
+#%%
+class DataGenerator(data.Dataset):
+    def __init__(self, u, y, s, 
+                 batch_size=64, rng_key=random.PRNGKey(1234)):
+        'Initialization'
+        self.u = u # input sample
+        self.y = y # location
+        self.s = s # labeled data evulated at y (solution measurements, BC/IC conditions, etc.)
+        
+        self.N = u.shape[0]
+    
+        self.batch_size = batch_size
+        self.key = rng_key
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        self.key, subkey = random.split(self.key)
+        inputs, outputs = self.__data_generation(subkey)
+        return inputs, outputs
+
+    @partial(jit, static_argnums=(0,))
+    def __data_generation(self, key):
+        'Generates data containing batch_size samples'
+        idx = random.choice(key, self.N, (self.batch_size,), replace=True)
+        s = self.s[idx,:]
+        y = self.y[idx,:]
+        u = self.u[idx,:]
+        # Construct batch
+        inputs = (u, y)
+        outputs = s
+        return inputs, outputs
+    
+#%%
+# Geneate training data corresponding to one input sample
+def generate_one_training_data(key, P, nu_max, sf):
+    (t, SIR), (u, y, s) = solve_SIR(key, P, length_scale = 2, nu_max = nu_max, sf=sf)
+    u = np.tile(u, (P, 1))
+    return u, y, s
+
+# Geneate test data corresponding to one input sample
+def generate_one_test_data(key, P, nu_max, sf):
+    (t, SIR), (u_test, y, s) = solve_SIR(key, P, length_scale = 2, nu_max = nu_max, sf = sf)
+
+    u_test = np.tile(u_test, (P, 1))
+    y_test = t.flatten()[:,None]
+    s_test = SIR.T.flatten()
+
+    return u_test, y_test, s_test
+
+# Geneate training data corresponding to N input sample
+def generate_training_data(key, N, P, nu_max, sf):
+    config.update("jax_enable_x64", True)
+    keys = random.split(key, N)
+    # u_train, y_train, s_train= vmap(generate_one_training_data, (0, None))(keys, P)
+    u_train_list, y_train_list, s_train_list = [], [], []
+
+    for k in keys:
+        u, y, s = generate_one_training_data(k, P, nu_max, sf)
+        u_train_list.append(u)
+        y_train_list.append(y)
+        s_train_list.append(s)
+
+    u_train = np.stack(u_train_list)
+    y_train = np.stack(y_train_list)
+    s_train = np.stack(s_train_list)
+
+    u_train = np.float32(u_train.reshape(N * P, -1))
+    y_train = np.float32(y_train.reshape(N * P, -1))
+    s_train = np.float32(s_train.reshape(N * P, -1))
+
+    config.update("jax_enable_x64", False)
+    return u_train, y_train, s_train
+
+# Geneate test data corresponding to N input sample
+def generate_test_data(key, N, P, nu_max, sf):
+    config.update("jax_enable_x64", True)
+    keys = random.split(key, N)
+    # u_test, y_test, s_test = vmap(generate_one_test_data, (0, None))(keys, P)
+    u_test_list, y_test_list, s_test_list = [], [], []
+
+    for k in keys:
+        u, y, s = generate_one_test_data(k, P, nu_max, sf)
+
+        u_test_list.append(u)
+        y_test_list.append(y)
+        s_test_list.append(s)
+
+    u_test = np.stack(u_test_list)
+    y_test = np.stack(y_test_list)
+    s_test = np.stack(s_test_list)
+
+    u_test = np.float32(u_test.reshape(N * P, -1))
+    y_test = np.float32(y_test.reshape(N * P, -1))
+    s_test = np.float32(s_test.reshape(-1, N * P)).T
+
+    config.update("jax_enable_x64", False)
+    return u_test, y_test, s_test
+
+#%% Data generator
+key = random.PRNGKey(0)
+
+# GRF length scale
+length_scale = 2
+
+# Resolution of the solution
+Nt = 100 # u의 사이즈랑 일치..
+N = 1000 # number of input samples
+m = Nt   # number of input sensors
+P_train = 500 # number of output sensors
+
+nu_max = 0.2
+sf = 'unif' # sampling field ('unif', 'gauss')
+
+#%% training data set
+u_train, y_train, s_train = generate_training_data(key, N, P_train, nu_max, sf)
+
+# training data set 를 파일에 저장
+with open('../data/data_u_train_unif_1.pkl', 'wb') as file:
+    pickle.dump(u_train, file)
+
+with open('../data/data_y_train_unif_1.pkl', 'wb') as file:
+    pickle.dump(y_train, file)
+
+with open('../data/data_s_train_unif_1.pkl', 'wb') as file:
+    pickle.dump(s_train, file)
+
+#%%
---
+++ src/model_sir.py
@@ -0,0 +1,84 @@
+#%%
+import numpy as np
+from scipy.integrate import odeint
+from scipy.interpolate import interp1d
+from jax import random
+
+#%%
+def solve_SIR(key, P, length_scale, nu_max = 1, sf = 'unif'):
+    """Solve SIR
+    St/dt = -beta*S*I - nu*S
+    It/dt = beta*S*I - gamma*I
+    Rt/dt = gamma*I + nu*S
+
+    P = location(y) 의 개수
+    """
+    # 초기 조건 설정
+    S0 = 0.999
+    I0 = 0.001
+    R0 = 0
+    y0 = [S0, I0, R0]
+    beta = 0.3
+    gamma = 1/14
+
+    # 시간 배열 설정
+    tmin, tmax = 0, 100 # days
+    dt = 1
+    t_span = np.arange(tmin, tmax, dt)
+    
+    # SIR 모델을 풀기 위한 함수
+    def SIR_model(y, t, beta, gamma, nu_func):
+        S, I, R = y
+        nu = nu_func(t)  # 시간에 따른 nu 값을 가져옵니다.
+        dSdt = -beta * S * I - nu * S
+        dIdt = beta * S * I - gamma * I
+        dRdt = gamma * I + nu * S
+        return [dSdt, dIdt, dRdt]
+
+    
+    # Generate subkeys
+    subkeys = random.split(key, 1)
+
+    # Generate a vaccine strategy sample
+    def gen_GRF(size, correlation_length, amplitude=1):
+        x = np.arange(size)
+        covariance_matrix = amplitude * np.exp(-0.5 * ((x[:, None] - x) / correlation_length)**2)
+        grf_sample = random.multivariate_normal(subkeys[0], np.zeros(size), covariance_matrix)
+
+        # Normalize to [0, 1]
+        grf_sample = nu_max *(grf_sample - grf_sample.min()) / (grf_sample.max() - grf_sample.min())
+        return grf_sample
+    
+    def gen_UFF(size,correlation_length):
+        import numpy as np
+        uni_sample = np.random.rand(size)
+        # Normalize to [0, 1]---------------------------------------------------------------------------------------
+        uni_sample = nu_max *(uni_sample - uni_sample.min()) / (uni_sample.max() - uni_sample.min())
+        return uni_sample
+
+    # Usage example:
+    sensor = tmax  # Size of the GRF
+    if sf == 'unif':
+        nu = gen_UFF(sensor, length_scale)
+    else:
+        nu = gen_GRF(sensor, length_scale)
+
+    def nu_function(t, nu):
+        t_min = 0
+        t_max = len(nu) - 1
+        t_scaled = (t - t_min) / (t_max - t_min)  # Scale t to the range [0, 1]
+        nu_interp = interp1d(np.linspace(0, 1, len(nu)), nu, kind='linear', fill_value='extrapolate')
+        return nu_interp(t_scaled)
+
+    f_fn = lambda t: nu_function(t, nu)
+    # 미분방정식 풀이
+    SIR = odeint(SIR_model, y0, t_span, args=(beta, gamma, f_fn))
+  
+    u = f_fn(t_span)
+    idx = random.randint(subkeys[1], (1, P), 0, len(t_span))
+    y = t_span[idx]
+    s = SIR[idx, :]
+
+    return (t_span, SIR), (u, y, s)
+
+# %%
